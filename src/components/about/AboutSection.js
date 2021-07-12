e593badb998715aab979d46969afd7d5150eb4c8
Val Currie <contact@valcuri.email>
2021-07-11 19:01:22 -0500

    refactoring

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Collapsible from "react-collapsible";
-import styled, { css } from "styled-components";
 import { config as getConfig } from "../../config";
+import {
+  AboutContainer,
+  AboutText,
+  AboutTextContainer,
+  AboutName,
+  AboutTextExpandText,
+  Avatar,
+} from "./styles";
 
 // see https://github.com/glennflanagan/react-collapsible/blob/develop/example/src/examples/ZeroHeightCollapsible.js
-export const AboutSection = (props) => {
+export const AboutSection = ({ expandedState, setExpandedState }) => {
   const [config] = useState(getConfig());
   const [githubUser, setGithubUser] = useState();
   const [, setRepos] = useState();
@@ -20,17 +27,15 @@ export const AboutSection = (props) => {
   }, [config]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const toggleExpanded = () => {
-    props.setExpandedState(!props.expandedState);
+    setExpandedState(!expandedState);
   };
 
   return (
     <AboutContainer>
-      <AvatarContainer>
-        <AvatarImage
-          src={`https://github.com/${config.github.username}.png`}
-          alt={`github avatar for user ${config.github.username}`}
-        />
-      </AvatarContainer>
+      <Avatar
+        src={`https://github.com/${config.github.username}.png`}
+        alt={`github avatar for user ${config.github.username}`}
+      />
       <AboutTextContainer>
         <AboutName>
           {config.about.name}
@@ -47,6 +52,7 @@ export const AboutSection = (props) => {
           trigger={
             <AboutText>
               {githubUser && `[${githubUser.location}] `}
+              {/* using \r\n depends on css`white-space: pre-wrap` */}
               {githubUser && githubUser.bio.replace("|| ", "\r\n")}
               <br /> <AboutTextExpandText />
             </AboutText>
@@ -58,78 +64,3 @@ export const AboutSection = (props) => {
     </AboutContainer>
   );
 };
-
-const AboutContainer = styled.div`
-  background: lavender;
-  display: flex;
-  flex-flow: row;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  min-width: 30rem;
-  box-shadow: 0px 0px 1px 1px darkgrey;
-`;
-
-const AboutTextContainer = styled.div`
-  padding-left: 4rem;
-  padding-right: 8rem;
-  display: flex;
-  flex-flow: column;
-  justify-content: space-around;
-  min-width: 27rem;
-`;
-
-const AboutName = styled.div`
-  font-size: 2rem;
-`;
-
-const AboutText = styled.div`
-  white-space: pre-wrap;
-  font-style: Oblique;
-  font-size: 1.1rem;
-  ${(props) =>
-    props.right &&
-    css`
-      text-align: right;
-    `}
-  ${(props) =>
-    props.center &&
-    css`
-      text-align: center;
-    `}
-`;
-
-const AboutTextExpandText = styled.span`
-  color: darkgrey;
-  margin-left: 4rem;
-  margin-right: 4rem;
-  ${AboutText}:hover & {
-    ::after {
-      content: "... click to show/hide...";
-    }
-  }
-`;
-
-const AvatarContainer = styled.div`
-  margin-top: 1rem;
-  margin-left: 2rem;
-  box-sizing: border-box;
-  border-radius: 50%;
-  border: 2px solid #ddd;
-  padding: 2px;
-  background-color: white;
-  max-width: 100%;
-  height: auto;
-  flex-shrink: 0.6;
-  flex-grow: 1;
-`;
-
-const AvatarImage = styled.img`
-  flex-shrink: inherit;
-  display: block;
-  border-radius: 50%;
-  width: 100%;
-  height: 100%;
-`;
